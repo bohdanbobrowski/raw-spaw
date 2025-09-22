b4c75ff1fc16fbbcc02b405347de3a8986032e96
Bohdan Bobrowski <bohdanbobrowski@gmail.com>
2025-09-22 23:42:12 +0200

    Preparation to reading exif.

--- Program.cs
+++ Program.cs
@@ -4,14 +4,53 @@ using System.Collections;
 
 class Program
 {
-    static void Main(string[] args)
+    static List<string> GetListOfRawFiles()
     {
+        List<string> rawFiles = new List<string>();
         string path = Directory.GetCurrentDirectory();
         Console.WriteLine("Listing files in {0} folder", path);
         string[] fileEntries = Directory.GetFiles(".");
         foreach (string fileName in fileEntries)
         {
-            Console.WriteLine(fileName);
+            if (fileName.EndsWith(".DNG"))
+                rawFiles.Add(fileName);
+        }
+        return rawFiles;
+    }
+
+    static void MoveRawFile(string rawFile)
+    {
+        string rawFileName = Path.GetFileNameWithoutExtension(rawFile);
+        var dirName = new DirectoryInfo(".").Name;
+        string destinationPath = Path.Combine(dirName, rawFileName + ".DNG");
+        if (!Directory.Exists("raw"))
+        {
+            // Directory.CreateDirectory("raw");
+            Console.WriteLine("Create directory: {0}", dirName);
+        }
+        // File.Move(rawFile, destinationPath);
+        Console.WriteLine("Move {0} to {1}", rawFile, destinationPath);
+    }
+
+    static void Main(string[] args)
+    {
+        List<string> rawFiles = GetListOfRawFiles();
+        foreach (string rawFile in rawFiles)
+        {
+            bool moveRaw = false;
+            string rawFileName = Path.GetFileNameWithoutExtension(rawFile);
+            if (!File.Exists(rawFileName + ".JPG"))
+            {
+                moveRaw = true;
+            } else
+            {
+                // TODO: read exif
+                Console.WriteLine("{0}.JPG exists", rawFileName);
+            }
+            if (moveRaw)
+            {
+                MoveRawFile(rawFile);
+            }
         }
     }
 }
